779e369f2b3ca802346accf257ee071a688bbdd7
RyanFrazin <RyanFrazin@Gmail.com>
2022-11-11 19:05:09 -0800

    Ryan Frazin Nov 11 2022, 19:05
    Initial commit contains large portion of already completed code.
    
    Add modal form to page initially hidden
    
    Add header and body element with grid flexibility
    
    Add css styles to all elements
    
    Add constructor property to script for new books
    
    Add functions for modal visibility when buttons on page are clicked
    
    Add functionality to addbook button on form which adds form inputs as new
    object to array myLibrary
    
    Add function checkLibrary() which creates new book object on DOM

---
+++ script.js
@@ -0,0 +1,69 @@
+const myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+//modal functions
+
+const modal = document.querySelector('.modal'); //modal form in doc
+
+const addButton = document.querySelector('#add');
+const closeButton = document.querySelector('.close');
+
+function pullDown() { //pulls down modal form when addBook button is pressed
+    if (modal.style.display === 'none') {
+        modal.style.display = 'flex';
+    } else {
+        modal.style.display = 'none';
+    }
+}
+addButton.addEventListener('click', pullDown);
+closeButton.addEventListener('click', pullDown);
+
+//form functions
+
+const formSubmit = document.querySelector('#addNew'); //SUBMIT FORM BUTTON
+
+let nameInput = document.getElementById('bookname');
+let authorInput = document.getElementById('author');
+let pageInput = document.getElementById('pages');
+
+formSubmit.addEventListener('click', function(){    //adds book to myLibrary array using form input values
+    let name = nameInput.value;
+    let author = authorInput.value;
+    let pageNum = pageInput.value;
+
+    let newBook = new Book(name, author, pageNum);
+    myLibrary.push(newBook);
+
+    checkLibrary(); //function adds book to grid element
+});
+
+
+//grid elements
+
+const gridElement = document.querySelector('.book-grid');
+
+function checkLibrary(){
+    let newestBook = myLibrary[myLibrary.length - 1];
+
+    const bookElement = document.createElement("div");
+    bookElement.classList.add('book');
+
+    const titleElement = document.createElement("h3");
+    titleElement.innerText = `Title: ${newestBook.title}`;
+    bookElement.appendChild(titleElement);
+
+    const authorElement = document.createElement("span");
+    authorElement.innerText = `Author: ${newestBook.author}`;
+    bookElement.appendChild(authorElement);
+
+    const pagesElement = document.createElement("span");
+    pagesElement.innerText = `Pages: ${newestBook.pages}`;
+    bookElement.appendChild(pagesElement);
+
+    gridElement.appendChild(bookElement);
+}
